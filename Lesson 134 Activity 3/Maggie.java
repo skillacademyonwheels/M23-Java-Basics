c1dba87a30a3a9b2ed38ac74142382dd34eb00ff
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-04 17:38:03 +0530

    Initial Commit

---
+++ Lesson 134 Activity 3/Maggie.java
@@ -0,0 +1,16 @@
+public class Maggie {
+    public static void main(String[] args) {
+        System.out.println("===== Methods in Java =====");
+        create_Maggie();
+    }
+
+
+public static void create_Maggie(){
+    System.out.println("Step 1: Boil water");
+    System.out.println("Step 2: Add Maggie noodles");
+    System.out.println("Step 3: Cook for 2 minutes");
+    System.out.println("Step 4: Add tastemaker");
+    System.out.println("Step 5: Stir well");
+    System.out.println("Step 6: Serve hot");
+}
+}
